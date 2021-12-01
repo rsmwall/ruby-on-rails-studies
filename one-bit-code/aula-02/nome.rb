02984628693808520313d8e434cc5d07125a8c7c
Rafael Silva <79131675+rsmwall@users.noreply.github.com>
2021-12-01 01:28:39 -0300

    Add files via upload

---
+++ one-bit-code/aula-02/nome.rb
@@ -0,0 +1,12 @@
+# imprime sem pular linha
+print "Digite seu nome: "
+
+# entrada de dados
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+
+sobrenome = gets.chomp
+
+# saida
+puts "Hello #{nome} #{sobrenome}!"
---
+++ one-bit-code/aula-02/pessoa.rb
@@ -0,0 +1,7 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "Olá #{nome}, você tem #{idade} anos!"
