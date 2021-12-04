bca6b942f25d8efc16bb7cecec491af9a1c50031
Rafael Silva <79131675+rsmwall@users.noreply.github.com>
2021-12-04 13:38:24 -0300

    Delete exercises/lista-01 directory

--- exercises/lista-01/velocidade-kilometro.rb
+++
@@ -1,8 +0,0 @@
-# Leia uma velocidade em km/h, calcule e escreva esta velocidade em m/s. (Vm/s = Vkm/h / 3.6)
-
-print "Digite a velocidade (Km/h): "
-vel_kilometro = gets.chomp.to_f
-
-vel_metro = vel_kilometro / 3.6
-
-puts "O resultado final é #{vel_metro} m/s!"
--- exercises/lista-01/velocidade-metro.rb
+++
@@ -1,8 +0,0 @@
-# Leia uma velocidade em m/s, calcule e escreva esta velocidade em km/h. (Vkm/h = Vm/s * 3.6)
-
-print "Digite a velocidade (m/s): "
-velo_metro = gets.chomp.to_f
-
-velo_kilo = velo_metro * 3.6
-
-puts "O resultado final é #{velo_kilo} Km/h!"
