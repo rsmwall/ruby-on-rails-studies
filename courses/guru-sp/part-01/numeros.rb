f66a64b2041c31e60e7cb1aa6eef80423a5bb6be
Rafael Silva <rafaelrsilva.dev@gmail.com>
2022-04-01 06:35:17 -0300

    Add files via upload

---
+++ courses/guru-sp/part-01/numeros.rb
@@ -0,0 +1,27 @@
+=begin
+    
+    Números em Ruby
+    Operadores comuns:
+
+    + adição
+    - subtração
+    * multiplicação
+    / divisão
+    
+=end
+
+puts 1 + 2
+puts 2 * 3
+
+=begin
+
+    Divisão de Inteiros
+    
+    Quando se faz um cálculo aritmético com inteiros,
+    recebe-se uma resposta em inteiros.
+
+=end
+
+puts 3 / 2
+puts 10 - 5
+puts 1.5 / 2.6
