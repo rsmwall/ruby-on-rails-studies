bac69b85dc15d5370b553adb8e4e23bc8f94f579
Rafael Silva <rafaelrsilva.dev@gmail.com>
2022-04-01 06:35:50 -0300

    Add files via upload

---
+++ courses/guru-sp/part-01/strings.rb
@@ -0,0 +1,30 @@
+# Em Ruby, Strings são mutáveis
+
+puts "Olá Mundo"
+puts 'Olá Mundo'
+
+# concatenação de strings
+
+puts 'Eu AMO' + ' Ruby!'
+
+# sequencia com escape
+
+puts 'It\'s my Ruby'
+
+# exibir a string n vezes (não faz a quebra de linha automática do puts)
+
+puts 'Olá' * 5
+
+# definindo uma constante
+
+PI = 3.1416
+puts PI
+
+=begin
+
+    strings que usam acento grave (`) são enviadas ao
+    S.O. como comando
+
+=end
+
+puts `ls` # linux e mac
