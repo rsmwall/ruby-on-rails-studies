3fd7bdb331604320383b001bdad8b33c41fb6708
Rafael Silva <rafaelrsilva.dev@gmail.com>
2022-05-18 22:06:44 -0300

    Add files via upload

---
+++ courses/onebitcode/challenges/02-basic-concepts/value-types.rb
@@ -0,0 +1,27 @@
+# crie um variavel para cada tipo
+
+# inteiro
+idade = 19
+
+# float
+salario = 4.800
+
+# string
+nome = "Rafael"
+
+# booleano
+solteiro = true
+
+# array
+frutas_favoritas = ['uva', 'maça', 'banana', 'melancia']
+
+# simbolo
+profissao = :programador
+
+# hash
+perfil = {
+    nome: 'Rafael',
+    profissao: 'Programador',
+    idade: '19',
+    pais: 'Brasil'
+}
