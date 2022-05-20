08fe47e286db58633ee17fa2a3a03ea127df83f2
Rafael Silva <rafaelrsilva.dev@gmail.com>
2022-05-19 22:41:48 -0300

    Add files via upload

---
+++ courses/onebitcode/04-collections/hash/each_hash.rb
@@ -0,0 +1,10 @@
+aulas = {
+  'Aula 1' => 'liberada',
+  'Aula 2' => 'liberada',
+  'Aula 3' => 'liberada',
+  'Aula 4' => 'liberada'
+}
+
+aulas.each do |key, value|
+  puts "#{key} - #{value}"
+end
