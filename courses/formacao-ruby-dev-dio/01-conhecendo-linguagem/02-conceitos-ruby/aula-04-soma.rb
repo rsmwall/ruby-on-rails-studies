fc5248683d4cc8c5be7d83b73ad000b9ee38ddc7
Rafael Silva <rafaelrsilva.dev@gmail.com>
2023-02-24 11:03:05 -0300

    Segunda parte do módulo 01 - conhecendo a linguagem

---
+++ courses/formacao-ruby-dev-dio/01-conhecendo-linguagem/02-conceitos-ruby/aula-04-soma.rb
@@ -0,0 +1,17 @@
+# usuario digite um numero
+# usuario digite outro numero
+# some os numeros
+
+# pedir ao usuario
+
+print "Digite um numero: "
+num1 = gets.chomp.to_i
+
+print "Digite outro numero: "
+num2 = gets.chomp.to_i
+
+soma = num1 + num2
+
+# aparecer na tela
+
+puts "A soma é igual a #{soma}"
---
+++ courses/formacao-ruby-dev-dio/01-conhecendo-linguagem/02-conceitos-ruby/aula-04.rb
@@ -0,0 +1,16 @@
+# 1. saida de dados na tela --> aparecer comando na tela
+# usuario digite seu nome
+
+print "Digite seu nome: "
+
+# 2. programa permita que o usuario digite
+# gets.chomp
+
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+# 3. saida final
+
+puts "Oi #{nome} #{sobrenome}"
