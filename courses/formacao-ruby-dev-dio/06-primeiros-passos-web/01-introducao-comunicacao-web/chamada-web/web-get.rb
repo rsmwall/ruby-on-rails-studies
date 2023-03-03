cf48ec03f3aa4b5c405e1f22e6fcb3e0c4546a48
Rafael Silva <rafaelrsilva.dev@gmail.com>
2023-03-03 04:09:25 -0300

    Primeira parte do módulo 06, introducao a web, com chamadas web, web-scrap e filesystem

---
+++ courses/formacao-ruby-dev-dio/06-primeiros-passos-web/01-introducao-comunicacao-web/chamada-web/web-get.rb
@@ -0,0 +1,9 @@
+# requisicao http
+
+require 'net/http'
+
+exemplo = Net::HTTP.get('example.com', '/index.html')
+
+File.open('example.html', 'w') do |line|
+  line.puts(exemplo)
+end
---
+++ courses/formacao-ruby-dev-dio/06-primeiros-passos-web/01-introducao-comunicacao-web/filesystem/write.rb
@@ -0,0 +1,4 @@
+File.open('lista.txt', 'a') do |line|
+  line.puts ('laranja')
+  line.puts ('batata')
+end
