22178c8cc9235ba82e695e1f32264ceee4551c0a
Rafael Silva <rafaelrsilva.dev@gmail.com>
2024-02-01 07:02:53 -0300

    Desafio 01 - Conhecendo Ruby

---
+++ courses/conhecendo-ruby/desafios/desafio_01.rb
@@ -0,0 +1,9 @@
+# Desafio 1
+# Declare duas variáveis, x e y, com respectivamente 1 e 2 como valores, com apenas uma
+# linha. Agora inverta os seus valores também com apenas uma linha de código. O que vamos
+# fazer aqui é uma atribuição em paralelo. Resposta no final do livro!
+
+a, b = 0, 1
+a, b = b, a
+
+puts a, b
