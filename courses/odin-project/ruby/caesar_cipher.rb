8e6945142bb198d3b46c1ce8f11b44c854cfb8e5
Rafael Silva <rafaelrsilva.dev@gmail.com>
2025-06-21 22:04:35 -0300

    refactor caesar_cipher

--- courses/odin-project/ruby/caesar_cipher.rb
+++ courses/odin-project/ruby/caesar_cipher.rb
@@ -1,22 +1,20 @@
-def caesar_cipher(phrase, steps)
-   low_alphabet = ('a'..'z').to_a
-   up_alphabet = ('A'..'Z').to_a
-   new_phrase = ""
-   phrase.chars.each do |char|
-      if ('a'..'z').include?(char) || ('A'..'Z').include?(char)
-         if char == char.upcase
-            index = up_alphabet.find_index(char)
-            new_phrase << up_alphabet[(index + steps) % 26]
-         else
-            index = low_alphabet.find_index(char)
-            new_phrase << low_alphabet[(index + steps) % 26]
-         end
-      else
-         new_phrase << char
-      end
-   end
+# frozen_string_literal: true
 
-   new_phrase
+def caesar_cipher(phrase, steps)
+  low_alphabet = ('a'..'z').to_a
+  up_alphabet = ('A'..'Z').to_a
+  phrase.chars.map do |char|
+    case char
+    when 'a'..'z' then get_new_char(low_alphabet, low_alphabet.find_index(char), steps)
+    when 'A'..'Z' then get_new_char(up_alphabet, up_alphabet.find_index(char), steps)
+    else
+      char
+    end
+  end.join
 end
 
-puts caesar_cipher("What a string!", 5)
+def get_new_char(alphabet, index, steps)
+  alphabet[(index + steps) % 26]
+end
+
+puts caesar_cipher('What a string!', 5)
